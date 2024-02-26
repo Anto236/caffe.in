1655c13e8ebd3904ae68e0460584a024753469c3
Anto236 <Antonymbugua1237@gmail.com>
2024-02-26 08:14:53 +0300

    updates

--- src/main/java/com/inn/cafe/constents/CafeConstants.java
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -6,4 +6,5 @@ public class CafeConstants {
     public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
     public static final String INVALID_DATA = "Invalid Data.";
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
+    public static  final String STORE_LOCATION = "C:\\Users\\hp\\Documents\\e_m\\JAVA\\caffeePdfs";
 }
